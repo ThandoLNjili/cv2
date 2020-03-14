cfff48d090355151466219d3a344afc288b75f33
Thandolwethu NJILI <tnjili@c3r2s3.wethinkcode.co.za>
2020-03-14 20:51:24 +0200

    Updates

---
+++ precache-manifest.8c5c24592bf577acd700a1cb99736938.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "192fe17510f0d0589342ac95cc0ba5f7",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "1863d8e692ee9c7182d4",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c7bef2962ea9e7735dce",
+    "url": "/react-gh-pages/static/js/2.932f9a4b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-gh-pages/static/js/2.932f9a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1863d8e692ee9c7182d4",
+    "url": "/react-gh-pages/static/js/main.135fe873.chunk.js"
+  },
+  {
+    "revision": "1defb6a1c82ed9b99ec4",
+    "url": "/react-gh-pages/static/js/runtime-main.b12e8238.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
